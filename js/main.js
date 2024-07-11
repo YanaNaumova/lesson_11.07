62f8e2de0a88361145fe7a953d4892885fb438f4
YanaNaumova <naumovayana87@gmail.com>
2024-07-11 10:29:16 +0200

    add menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 // Создать навигационное меню, которое будет открываться, выезжая из края экрана.
 // Само меню - абсолютно позиционированный блок с заданным свойством transform: translateX,
 // которое будет меняться в зависимости от состояния меню.
+
+const menu = document.querySelector(".menu");
+const cancel = document.querySelector(".cancel");
+const nav = document.querySelector("nav");
+
+menu.addEventListener("click", () => {
+  nav.classList.remove("hiddenNav");
+  nav.classList.add("showNav");
+});
+
+cancel.addEventListener("click", () => {
+  nav.classList.remove("showNav");
+  nav.classList.add("hiddenNav");
+});
